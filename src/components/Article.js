ec58ab69280611f415294828d4ec1f65634ac200
naliali <nalialiangela365@gmail.com>
2024-04-22 18:17:24 +0300

    running tests

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,13 @@
-import React from "react";
+import "@testing-library/jest-dom"
+import { render, screen } from '@testing-library/react';
+import Article from '..components/Article';
+import React from "react"
 
-function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
-}
 
+test('displays the test "please pass this test"', () => {
+  render(<Article/>);
+
+  screen.debug();
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
 export default Article;
